9ab69379d4857a2aef3754fc9fd539003d0719e1
LH <me@lholota.com>
2023-04-27 23:24:55 +0200

    fix: diffSopsFiles function missing export (#7)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const diffSopsFiles = require("./diffSopsFiles")
 const diffFiles = require("./diffFiles")
 const diffDirs = require("./diffDirs")
 
 module.exports = {
+    diffSopsFiles,
     diffFiles,
     diffDirs
 }
